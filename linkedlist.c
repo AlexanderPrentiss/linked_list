062077c706a004630ba607f81c0950a6e5596ffa
AlexanderPrentiss <119530838+AlexanderPrentiss@users.noreply.github.com>
2025-01-11 13:55:25 -0500

    commented code, outlined todos, seperated test function from main

--- linkedlist.c
+++ linkedlist.c
@@ -1,56 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Node struct
 typedef struct node{
   int data;
   struct node* next;
 } Node_T;
 
+// --Node methods--
+// set data changes the data attribute
 void set_data(Node_T* node, int num) {
   node->data = num;
 }
 
+// set_next changes the pointer to next of type node attribute
 void set_next(Node_T* node, Node_T* next) {
   node->next = next;
 }
 
+// Linked List struct
 typedef struct linkedlist{
   int size;
   Node_T* head;
 } LinkedList_T;
 
+// --Linked List methods--
+// TODO: add_node creates nodes instead of being given nodes to insert
+// add_node appends a node object to the node list attribute
 void add_node(LinkedList_T* list, Node_T* node){
-  if (list->head == NULL) {
+  if (list->head == NULL) {     // if the list is empty make the current node the head
     list->head = node;
     list->size = 1;
-  } else {
+  } else {                      // if the list is not empty find the end and append node
     Node_T* cur = list->head;
     while (cur->next != NULL) {
       cur = cur->next;
     }
     set_next(cur, node);
     list->size++;
-    //free(cur);
   }
 }
 
+//get_node returns the node of a specified value
 Node_T* get_node(LinkedList_T* list,int data) {
-  if (list->size <= 0) {
+  if (list->size <= 0) {            // if the list is empty it's probably not there
     printf("Item not in list\n");
     return NULL;
-  } else {
+  } else {                          // if it isn't empty loop through the list to check
     Node_T* cur = list->head;
     while (cur->next != NULL) {
-      if (cur->data == data) return cur;
+      if (cur->data == data) return cur; // found!
       cur = cur->next;
     }
-    //free(cur);
   }
   printf("Item not in list\n");
-  return NULL;
+  return NULL;                      // still not there 
 }
 
-void destroy_list(LinkedList_T* list) {
+// TODO:
+// destroy_list will free each node object's memory, then free the list
+/*void destroy_list(LinkedList_T* list) {
   Node_T* cur = list->head;
   Node_T* to_free;
   while (cur != NULL) {
@@ -59,9 +68,10 @@ void destroy_list(LinkedList_T* list) {
     //free(to_free);
   }
   free(cur);
-}
+}*/
 
-int main(void){
+// test checks the functionality of the list
+void test(){
   Node_T* node1 = (Node_T*) malloc(sizeof(Node_T));
   Node_T* node2 = (Node_T*) malloc(sizeof(Node_T));
   Node_T* node3 = (Node_T*) malloc(sizeof(Node_T));
@@ -78,5 +88,9 @@ int main(void){
   printf("Should be NULL: %p\n", get_node(list, 25));
   printf("Should be some pointer: %p\n", get_node(list,2));
   //destroy_list(list);
+}
+
+int main() {
   return 0;
 }
+
